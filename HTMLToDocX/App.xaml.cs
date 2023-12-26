e07c419b46fca45243dd73531f9bda58fb36e9f2
aiqinxuancai <aiqinxuancai@163.com>
2023-12-26 15:11:36 +0800

    新的方案HTMLToDocX

---
+++ HTMLToDocX/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using HTMLToDocX.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using Wpf.Ui;
+
+namespace HTMLToDocX
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static readonly IHost _host = Host.CreateDefaultBuilder()
+        .ConfigureAppConfiguration(c =>
+        {
+            c.SetBasePath(AppContext.BaseDirectory);
+        })
+        .ConfigureServices(
+            (context, services) =>
+            {
+                services.AddSingleton<ISnackbarService, SnackbarService>();
+                services.AddSingleton<IContentDialogService, ContentDialogService>();
+            }
+        )
+        .Build();
+
+        /// <summary>
+        /// Gets registered service.
+        /// </summary>
+        /// <typeparam name="T">Type of the service to get.</typeparam>
+        /// <returns>Instance of the service or <see langword="null"/>.</returns>
+        public static T? GetService<T>() where T : class
+        {
+            return _host.Services.GetService(typeof(T)) as T ?? null;
+        }
+
+        App()
+        {
+            AppConfig.Instance.Init();
+        }
+
+
+    }
+
+}
---
+++ HTMLToDocX/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using Aspose.Words;
+using HTMLToDocX.Utils;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using Wpf.Ui;
+using Wpf.Ui.Controls;
+using Xceed.Words.NET;
+
+namespace HTMLToDocX
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FluentWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Wpf.Ui.Appearance.SystemThemeWatcher.Watch(this);
+        }
+
+        private async void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+
+            ButtonStart.IsEnabled = false;
+            var path = TextBoxPath.Text;
+
+
+            UploadProgressView.Visibility = Visibility.Visible;
+
+
+            await Task.Run(() => {
+                StartWithPath(path);
+            });
+
+
+            UploadProgressView.Visibility = Visibility.Collapsed;
+            ButtonStart.IsEnabled = true;
+
+        }
+
+        private async void StartWithPath(string path)
+        {
+            var docxPath = Path.Combine(path, "DOCX");
+
+            if (Directory.Exists(path))
+            {
+                var files = Directory.GetFiles(path, "*.html");
+
+
+                var successCount = 0;
+                foreach (var item in files)
+                {
+                    try
+                    {
+                        //GlobalNotification.Default.Post(GlobalNotification.NotificationOutputLogInfo, $"正在处理：{Path.GetFileName(item)}");
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            UploadProgressView.Status = $"正在处理：{Path.GetFileName(item)}";
+                        });
+                        
+
+                        var document = new Aspose.Words.Document(item);
+
+                        var newPath = Path.Combine(docxPath, Path.GetFileName(item) + ".docx");
+
+                        // 将 HTML 文件转换为 Word DOCX 格式
+                        document.Save(newPath, SaveFormat.Docx);
+
+                        // 移除部分信息
+                        DocXHelper.ModifyDocument(newPath, "Aspose.Words");
+
+                        successCount++;
+                    }
+                    catch (Exception ex)
+                    {
+
+
+                    }
+
+                }
+
+                await this.Dispatcher.Invoke(async () =>
+                {
+                    var service = App.GetService<IContentDialogService>();
+                    service.SetContentPresenter(DialogPresenter);
+                    var result = await service.ShowSimpleDialogAsync(
+                        new SimpleContentDialogCreateOptions()
+                        {
+                            Title = "成功",
+                            Content = "完成，处理完成的HTML存储在\\DOCX\r\n只需将.docx文件导入Notion既可",
+                            PrimaryButtonText = "打开目录",
+                            CloseButtonText = "关闭",
+                        }
+                    );
+
+                    if (result == ContentDialogResult.Primary)
+                    {
+                        //打开目录
+                        Process.Start("explorer.exe", docxPath);
+            
+                    }
+
+                });
+
+            }
+            else
+            {
+                await this.Dispatcher.Invoke(async () =>
+                {
+                    var service = App.GetService<IContentDialogService>();
+                    service.SetContentPresenter(DialogPresenter);
+                    var result = await service.ShowSimpleDialogAsync(
+                        new SimpleContentDialogCreateOptions()
+                        {
+                            Title = "失败",
+                            Content = "输入的目录不存在",
+                            CloseButtonText = "取消",
+                        }
+                    );
+                });
+
+            }
+        }
+
+    }
+}
